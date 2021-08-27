842053fe8e4338c4b7a66fe3a98568ffdc4af4fa
Hidetake Iwata <int128@gmail.com>
2021-08-28 08:25:10 +0900

    Use eslint:recommended

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,12 @@ import * as core from '@actions/core'
 import { run } from './run'
 
 const main = async (): Promise<void> => {
-  try {
-    await run({
-      authors: core.getMultilineInput('authors'),
-      startsWith: core.getMultilineInput('starts-with'),
-      endsWith: core.getMultilineInput('ends-with'),
-      token: core.getInput('token', { required: true }),
-    })
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+  await run({
+    authors: core.getMultilineInput('authors'),
+    startsWith: core.getMultilineInput('starts-with'),
+    endsWith: core.getMultilineInput('ends-with'),
+    token: core.getInput('token', { required: true }),
+  })
 }
 
-main()
+main().catch((error) => core.setFailed(error))
