48c2eadb6bfc7e4f3c43fd070a63ca2218428ca6
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2025-10-13 08:28:17 +0000

    Generated by GitHub Actions (ts / generate)
    https://github.com/int128/hide-comment-action/actions/runs/18459747004

--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -1,43 +1,17 @@
-import type * as Types from './graphql-types.js'
+import * as Types from './graphql-types.js';
 
 export type CommentsQueryVariables = Types.Exact<{
-  owner: Types.Scalars['String']['input']
-  name: Types.Scalars['String']['input']
-  number: Types.Scalars['Int']['input']
-}>
+  owner: Types.Scalars['String']['input'];
+  name: Types.Scalars['String']['input'];
+  number: Types.Scalars['Int']['input'];
+}>;
 
-export type CommentsQuery = {
-  __typename?: 'Query'
-  repository?: {
-    __typename?: 'Repository'
-    pullRequest?: {
-      __typename?: 'PullRequest'
-      comments: {
-        __typename?: 'IssueCommentConnection'
-        nodes?: Array<{
-          __typename?: 'IssueComment'
-          id: string
-          url: string
-          body: string
-          isMinimized: boolean
-          author?:
-            | { __typename?: 'Bot'; login: string }
-            | { __typename?: 'EnterpriseUserAccount'; login: string }
-            | { __typename?: 'Mannequin'; login: string }
-            | { __typename?: 'Organization'; login: string }
-            | { __typename?: 'User'; login: string }
-            | null
-        } | null> | null
-      }
-    } | null
-  } | null
-}
+
+export type CommentsQuery = { __typename?: 'Query', repository?: { __typename?: 'Repository', pullRequest?: { __typename?: 'PullRequest', comments: { __typename?: 'IssueCommentConnection', nodes?: Array<{ __typename?: 'IssueComment', id: string, url: string, body: string, isMinimized: boolean, author?: { __typename?: 'Bot', login: string } | { __typename?: 'EnterpriseUserAccount', login: string } | { __typename?: 'Mannequin', login: string } | { __typename?: 'Organization', login: string } | { __typename?: 'User', login: string } | null } | null> | null } } | null } | null };
 
 export type MinimizeCommentMutationVariables = Types.Exact<{
-  id: Types.Scalars['ID']['input']
-}>
+  id: Types.Scalars['ID']['input'];
+}>;
 
-export type MinimizeCommentMutation = {
-  __typename?: 'Mutation'
-  minimizeComment?: { __typename?: 'MinimizeCommentPayload'; clientMutationId?: string | null } | null
-}
+
+export type MinimizeCommentMutation = { __typename?: 'Mutation', minimizeComment?: { __typename?: 'MinimizeCommentPayload', clientMutationId?: string | null } | null };
